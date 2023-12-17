c85792756d847756b7c1bc06c48029dd0e5de812
Kevin Bruner-Prime <kevin.a.bruner@gmail.com>
2023-12-17 13:32:39 -0500

    updating script to update apt

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 #installs dependencies
+sudo DEBIAN_FRONTEND=noninteractive apt-get update -yq
+sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -yq
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -yq apache2 mysql-server php php-gd php-pdo php-mysql php-dom ncdu gh composer vim nfs-common
 
 sudo chown -R kevin:www-data /var/www
