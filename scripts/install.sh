2e44c27dc1419255da450b798ce4847e7905c58a
Kevin Bruner-Prime <kevin@rae-dev.thejfk.ca>
2023-12-17 18:20:29 +0000

    more install bits

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 #installs dependencies
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -yq apache2 mysql-server php php-gd php-pdo php-mysql php-dom ncdu gh composer
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -yq apache2 mysql-server php php-gd php-pdo php-mysql php-dom ncdu gh composer vim nfs-common
 
 sudo chown -R kevin:www-data /var/www
+cp composer.json composer.lock /var/www/
+sudo rm -R /var/www/html
 cd /var/www/
 
-cp composer.json composer.lock /var/www/
 composer install
 cd -
 cp -R themes/* /var/www/html/themes/
+
+ln -s /var/www/web /var/www/html
 
 #database import
 
