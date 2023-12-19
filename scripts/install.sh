7af6b34db6f68cd9730877b3304120f42833c338
Kevin Bruner-Prime <kevin.a.bruner@gmail.com>
2023-12-18 23:02:54 -0500

    different call

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,9 +8,9 @@ password=$3
 gitDir=$4
 application=$5
 
-./install_deps.sh
-./mount_nfs.sh $linuxUser
-./composer_install.sh
-./populate_db.sh $dbName $username $password $linuxUser
-./configure_apache.sh $linuxUser $gitDir
-./configure_drupal.sh $dbName $username $password $gitDir $linuxUser
+bash $gitDir/scripts/install_deps.sh
+bash $gitDir/scripts/mount_nfs.sh $linuxUser
+bash $gitDir/scripts/composer_install.sh
+bash $gitDir/scripts/populate_db.sh $dbName $username $password $linuxUser
+bash $gitDir/scripts/configure_apache.sh $linuxUser $gitDir
+bash $gitDir/scripts/configure_drupal.sh $dbName $username $password $gitDir $linuxUser
