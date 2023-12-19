6cf14613afac2626dd6abb82bfc6418b7f7e0a9e
Kevin Bruner-Prime <kevin.a.bruner@gmail.com>
2023-12-18 23:06:11 -0500

    forgot to pass variables

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,8 +9,8 @@ gitDir=$4
 application=$5
 
 bash $gitDir/scripts/install_deps.sh
-bash $gitDir/scripts/mount_nfs.sh $linuxUser
-bash $gitDir/scripts/composer_install.sh
+bash $gitDir/scripts/mount_nfs.sh $linuxUser $application
+bash $gitDir/scripts/composer_install.sh $linuxUser $gitDir
 bash $gitDir/scripts/populate_db.sh $dbName $username $password $linuxUser
 bash $gitDir/scripts/configure_apache.sh $linuxUser $gitDir
 bash $gitDir/scripts/configure_drupal.sh $dbName $username $password $gitDir $linuxUser
