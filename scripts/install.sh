2050e774f0c8b9c7a16b7c1f904fc4f61c4708bc
Kevin Bruner-Prime <kevin.a.bruner@gmail.com>
2023-12-18 23:30:43 -0500

    errant quote

--- scripts/install.sh
+++ scripts/install.sh
@@ -15,7 +15,7 @@ linuxUser=$SUDO_USER
 bash $gitDir/scripts/set_env_vars.sh "$linuxUser" "$gitDir" "$application" "$branch"
 bash $gitDir/scripts/install_deps.sh 
 bash $gitDir/scripts/mount_nfs.sh 
-bash $gitDir/scripts/composer_install.sh "
+bash $gitDir/scripts/composer_install.sh 
 bash $gitDir/scripts/populate_db.sh "$dbName" "$username" "$password" 
 bash $gitDir/scripts/configure_apache.sh
 bash $gitDir/scripts/configure_drupal.sh "$dbName" "$username" "$password"
