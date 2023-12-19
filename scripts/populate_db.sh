ec890026a7e2bcc71ce20d4902039dec38a9058b
Kevin Bruner-Prime <kevin.a.bruner@gmail.com>
2023-12-18 23:42:52 -0500

    adding a crontab for main

--- scripts/populate_db.sh
+++ scripts/populate_db.sh
@@ -21,3 +21,23 @@ if [ -z "$databaseExists" ]; then
 else
   echo "Database already exists. Skipping creation and import."
 fi
+
+
+
+###crontab manipulation for DB backups****
+
+scriptPath=$gitDir/scripts/db_dump.sh
+
+# Check if the current branch is main
+if [ "$branch" = "refs/heads/main" ]; then
+  # Check if the cron job already exists
+  if ! crontab -l | grep -q "$scriptPath"; then
+    # Add the cron job to run the script every night at 2 AM
+    (crontab -l ; echo "0 2 * * * $scriptPath") | crontab -
+    echo "Cron job added to run $scriptPath every night at 2 AM."
+  else
+    echo "Cron job already exists for $scriptPath."
+  fi
+else
+  echo "The branch is not main. Skipping cron job addition."
+fi
