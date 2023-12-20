0a94050b777f9ca5a17386f4b894ab8ce0cadbea
Kevin Bruner-Prime <kevin.a.bruner@gmail.com>
2023-12-20 11:03:48 -0500

    different logic

--- scripts/populate_db.sh
+++ scripts/populate_db.sh
@@ -4,23 +4,29 @@ source /etc/environment
 ###database import###
 sqlFile="/home/$linuxUser/db-dumps/$dbName.sql" 
 
-# Check if the database exists
-databaseExists=$(mysql -e "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME='$dbName';" -s)
+# Check if the SQL file exists
+if [ -f "$sqlFile" ]; then
+    # Check if the database exists
+    databaseExists=$(mysql -e "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME='$dbName';" -s)
 
-if [ -f "$sqlFile" ] && [ -z "$databaseExists" ]; then
-    # Create the database
-    mysql -e "CREATE DATABASE IF NOT EXISTS $dbName;"
+    if [ -z "$databaseExists" ]; then
+        # Create the database
+        mysql -e "CREATE DATABASE IF NOT EXISTS $dbName;"
 
-    #mysql user
-    mysql -e "CREATE USER '$username'@'localhost' IDENTIFIED BY '$password';"
-    mysql -e "GRANT ALL PRIVILEGES ON $dbName.* TO '$username'@'localhost';"
-    mysql -e "FLUSH PRIVILEGES;"
+        #mysql user
+        mysql -e "CREATE USER '$username'@'localhost' IDENTIFIED BY '$password';"
+        mysql -e "GRANT ALL PRIVILEGES ON $dbName.* TO '$username'@'localhost';"
+        mysql -e "FLUSH PRIVILEGES;"
 
-    # Import data into the database from SQL file
-    mysql -u"$username" -p"$password" "$dbName" < "$sqlFile"
+        # Import data into the database from SQL file
+        mysql -u"$username" -p"$password" "$dbName" < "$sqlFile"
+    else
+        echo "Database already exists. Skipping creation and import."
+    fi
 else
-  echo "Database already exists. Skipping creation and import."
+    echo "SQL file $sqlFile does not exist. Database import cannot proceed."
 fi
+
 
 
 ###crontab manipulation for DB backups****
