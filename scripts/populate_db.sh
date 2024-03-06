de64cad11b92f9dac56a96f3f2c92f2124da11e3
Kevin Bruner-Prime <kevin.a.bruner@gmail.com>
2024-03-06 18:55:49 -0500

    removing db population

--- scripts/populate_db.sh
+++ scripts/populate_db.sh
@@ -4,26 +4,6 @@ source /etc/environment
 ###database import###
 sqlFile="/home/$linuxUser/db-dumps/$dbName.sql" 
 
-if [ -f "$sqlFile" ]; then
-    # Check if the database exists for first-time setup only
-    databaseExists=$(mysql -e "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME='$dbName';" -s)
-
-    if [ -z "$databaseExists" ]; then
-        # Create the database
-        mysql -e "CREATE DATABASE IF NOT EXISTS $dbName;"
-
-        #mysql user
-        mysql -e "CREATE USER '$username'@'localhost' IDENTIFIED BY '$password';"
-        mysql -e "GRANT ALL PRIVILEGES ON $dbName.* TO '$username'@'localhost';"
-        mysql -e "FLUSH PRIVILEGES;"
-
-        # Import data into the database from SQL file
-        mysql -u"$username" -p"$password" "$dbName" < "$sqlFile"
-    else
-        echo "Database already exists. Skipping creation and import."
-    fi
-fi
-
 ###crontab manipulation for DB backups****
 scriptPath=$gitDir/scripts/db_dump.sh
 
