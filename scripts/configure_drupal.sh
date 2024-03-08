ea6d058639d77f1ce6a5576e3797912d9e55a0e6
Kevin Bruner-Prime <kevin.a.bruner@gmail.com>
2024-03-08 16:14:15 -0500

    wrong var declarations

--- scripts/configure_drupal.sh
+++ scripts/configure_drupal.sh
@@ -7,9 +7,9 @@ cd $settingsDir
 cp $gitDir/settings.php $settingsDir/
 
 if [[ ${server} == 'prod1' || ${server} == 'prod2' ]]; then
-    $dbHost="192.168.11.40"
+    dbHost="192.168.11.40"
 else
-    $dbHost="192.168.11.50"
+    dbHost="192.168.11.50"
 fi
 
 # Replace values in settings.php
