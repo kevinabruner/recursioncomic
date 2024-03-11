77cd005fe666efdb2ebe1bd512a1736cb30d6800
Kevin Bruner-Prime <kevin.a.bruner@gmail.com>
2024-03-11 08:39:02 -0400

    mounting nfs first

--- scripts/install.sh
+++ scripts/install.sh
@@ -65,8 +65,8 @@ update_env "server" "$server"
 ###scriplets to install application***
 bash $gitDir/scripts/pre_install.sh 
 bash $gitDir/scripts/install_deps.sh 
-bash $gitDir/scripts/composer_install.sh 
 bash $gitDir/scripts/mount_nfs.sh 
+bash $gitDir/scripts/composer_install.sh 
 bash $gitDir/scripts/populate_db.sh 
 bash $gitDir/scripts/configure_apache.sh
 bash $gitDir/scripts/configure_drupal.sh 
