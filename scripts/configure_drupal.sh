af0bae874e201ff72a911abbb9134f80ecaee993
Kevin Bruner-Prime <kevin.a.bruner@gmail.com>
2024-03-20 00:27:05 -0400

    skip drush

--- scripts/configure_drupal.sh
+++ scripts/configure_drupal.sh
@@ -23,8 +23,8 @@ sudo chown -R $linuxUser:www-data /var/www
 
 #updates using drush
 cd /var/www
-drush='/var/www/vendor/drush/drush/drush'
-$drush cr -vvv
+#drush='/var/www/vendor/drush/drush/drush'
+#$drush cr -vvv
 sudo -u $linuxUser -g www-data $drush updb -vvv
 
 #set perms on ceph files
