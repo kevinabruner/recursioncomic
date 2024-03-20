b3aed4538c19f27af051eb974ef36c716cf5a762
Kevin Bruner-Prime <kevin.a.bruner@gmail.com>
2024-03-20 08:21:06 -0400

    putting back drush

--- scripts/configure_drupal.sh
+++ scripts/configure_drupal.sh
@@ -24,6 +24,7 @@ sudo chown -R $linuxUser:www-data /var/www
 #updates using drush
 cd /var/www
 drush='/var/www/vendor/drush/drush/drush'
+
 $drush cr -vvv
 sudo -u $linuxUser -g www-data $drush updb -vvv
 
