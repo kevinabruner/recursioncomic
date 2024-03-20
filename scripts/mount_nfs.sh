8c4a26fb199a1b4de7d101e81b3f437a0ba7263a
Kevin Bruner-Prime <kevin.a.bruner@gmail.com>
2024-03-19 22:23:34 -0400

    gotta make the backupdir

--- scripts/mount_nfs.sh
+++ scripts/mount_nfs.sh
@@ -3,7 +3,7 @@ source /etc/environment
 
 backupDir="/home/$linuxUser/nfs-backups"
 
-sudo mkdir -p 
+sudo mkdir -p $backupDir
 sudo chown $linuxUser:www-data $entry
 nfsEntry="192.168.11.20:/mnt/yes/proxmox/gitbuilds/$application  $backupDir  nfs    defaults    0 0"
 # Check if the entry exists in /etc/fstab
