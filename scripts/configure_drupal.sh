6527d992f115d7156e7944165505f1b6f6407896
Kevin Bruner-Prime <kevin.a.bruner@gmail.com>
2024-03-23 11:00:13 -0400

    removing perms

--- scripts/configure_drupal.sh
+++ scripts/configure_drupal.sh
@@ -29,8 +29,3 @@ cd /var/www
 #$drush cr -vvv
 
 sudo -u $linuxUser -g www-data $drush updb -vvv
-
-#set perms on ceph files
-cephFiles="/var/www/web/sites/default/files"
-sudo chown -R $linuxUser:www-data $cephFiles
-sudo chmod 775 -R $linuxUser:www-data $cephFiles
